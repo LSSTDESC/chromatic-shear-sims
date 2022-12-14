e55861bebb16ef0f64cf84414079dd0508a7c0cd
Sidney Mau <sidneymau@gmail.com>
2022-12-13 17:01:36 -0800

    Add in-progress script for validating SEDs

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import setup, find_packages
 scripts = [
     "bin/run-chromatic-shear-bias",
     "bin/meas-chromatic-shear-bias",
+    "bin/validate-seds",
     "bin/viz-chromatic-shear-bias",
 ]
 
