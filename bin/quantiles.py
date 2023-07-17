e1b4121137669250b4c20ef8d7e9ddc7f3705c35
Sidney Mau <smau@stanford.edu>
2023-07-17 10:54:14 -0700

    update quantile script

--- bin/quantiles.py
+++ bin/quantiles.py
@@ -27,16 +27,33 @@ def main():
 
     dataset = ds.dataset("/oak/stanford/orgs/kipac/users/smau/cosmoDC2_v1.1.4_parquet")
     table = dataset.to_table(columns=columns, filter=predicate)
-    # quantiles = [0.0, 1./3., 2./3., 1.0]  # tertiles
-    # quantiles = [0.0, 0.25, 0.5, 0.75, 1.0]  # quartiles
-    # quantiles = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]  # quintiles
     quantiles = args.quantiles
 
+    print(f"Gals")
     print(f"Calculating median")
     print(pc.approximate_median(pc.subtract(table["mag_true_g_lsst"], table["mag_true_i_lsst"])))
 
     print(f"Calculating quantiles: {quantiles}")
     print(pc.quantile(pc.subtract(table["mag_true_g_lsst"], table["mag_true_i_lsst"]), quantiles))
+
+    columns = [
+      "gmag",
+      "imag",
+    ]
+    predicate = (
+        (pc.field("imag") > pc.scalar(19))
+        & (pc.field("imag") < pc.scalar(21))
+        & pc.match_substring_regex(pc.field("sedFilename"), "^k[mp]*")
+    )
+    dataset = ds.dataset("/oak/stanford/orgs/kipac/users/smau/dc2_stellar_healpixel.arrow", format="arrow")
+    table = dataset.to_table(columns=columns, filter=predicate)
+
+    print(f"stars")
+    print(f"Calculating median")
+    print(pc.approximate_median(pc.subtract(table["gmag"], table["imag"])))
+
+    print(f"Calculating quantiles: {quantiles}")
+    print(pc.quantile(pc.subtract(table["gmag"], table["imag"]), quantiles))
 
 if __name__ == "__main__":
     main()
