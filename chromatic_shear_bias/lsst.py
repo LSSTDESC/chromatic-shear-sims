fdb09ddba21d369dec67e7aa2db48cca4f7b0706
Sidney Mau <sidneymau@gmail.com>
2023-12-13 15:55:57 -0800

    update lsst constants

--- chromatic_shear_bias/lsst.py
+++ chromatic_shear_bias/lsst.py
@@ -1,4 +1,28 @@
-zeropoint = {
+import numpy as np
+
+import galsim
+
+
+# cf.
+# 	https://lsst.org/scientists/keynumbers
+# 	https://smtn-002.lsst.io/
+# 	https://pstn-054.lsst.io/
+
+SCALE = 0.2  # arcsec/pixel
+EXPTIME = 30  # s
+AREA = np.pi * (6.423 / 2)**2 * 1e4  # cm^2
+GAIN = 1  # photons/ADU
+
+NCOADD = {
+    "u": 100,
+    "g": 100,
+    "r": 100,
+    "i": 100,
+    "z": 100,
+    "y": 100,
+}
+
+ZEROPOINTS = {
     "u": 27.03,
     "g": 28.38,
     "r": 28.16,
@@ -7,7 +31,7 @@ zeropoint = {
     "y": 26.68,
 }
 
-sky_brightness = {
+SKY = {
     "u": 22.96,
     "g": 22.26,
     "r": 21.20,
@@ -16,6 +40,12 @@ sky_brightness = {
     "y": 18.61,
 }
 
-exposure_time = 30
+SKY_RMS = {
+    f: np.power(10, -0.4 * (SKY[f] - ZEROPOINTS[f])) * EXPTIME * SCALE
+    for f in SKY.keys()
+}
 
-pixel_scale = 0.2
+BANDPASSES = {
+    band: galsim.Bandpass(f"LSST_{band}.dat", "nm").withZeropoint("AB")
+    for band in {"u", "g", "r", "i", "z", "y"}
+}
