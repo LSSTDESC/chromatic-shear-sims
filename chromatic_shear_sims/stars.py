1cadd5bc768a31fb42a29bc9bd7598a9b8599e54
Sidney Mau <sidneymau@gmail.com>
2024-08-21 12:38:11 -0700

    add wrapper class for stars

---
+++ chromatic_shear_sims/stars.py
@@ -0,0 +1,73 @@
+import logging
+import time
+
+import galsim
+import numpy as np
+
+from chromatic_weak_lensing import MainSequence
+
+from chromatic_shear_sims import utils
+
+
+logger = logging.getLogger(__name__)
+
+
+class Stars:
+    def __init__(self, module_name, class_name):
+        self.model = utils.get_instance(module_name, class_name)
+        self.name = self.model.name
+
+    def __call__(self, stellar_params):
+        model_params = model.get_params(stellar_params)
+        star = model.get_star(*model_params)
+        return star
+
+
+class InterpolatedStars:
+    def __init__(self, module_name, class_name, throughput_1, throughput_2):
+        self.model = utils.get_instance(module_name, class_name)
+        self.name = self.model.name
+        self.lut = self.get_lut(throughput_1, throughput_2)
+        self.x_min = self.lut.x_min
+        self.x_max = self.lut.x_max
+
+    def get_lut(self, throughput_1, throughput_2, m_min=0.1, m_max=10.0, n=1_000):
+        """
+        Get a lookup table so as to find the mass of a star
+        whose spectra produces a given color.
+        """
+        _start_time = time.time()
+        masses = np.geomspace(m_min, m_max, n)
+
+        colors = []
+        for mass in masses:
+            sparams = MainSequence.get_params(mass)
+            params = self.model.get_params(sparams)
+            spec = self.model.get_spectrum(*params)
+            color = spec.calculateMagnitude(throughput_1) - spec.calculateMagnitude(throughput_2)
+            colors.append(color)
+
+        lut = galsim.LookupTable(colors, masses, x_log=False, f_log=True, interpolant="linear")
+        _end_time = time.time()
+        _elapsed_time = _end_time - _start_time
+        logger.info(f"made inverse lookup table in {_elapsed_time} seconds")
+
+        return lut
+
+    def get_spec(self, color):
+        mass = self.lut(color)
+        sparams = MainSequence.get_params(mass)
+        params = self.model.get_params(sparams)
+        spec = self.model.get_spectrum(*params)
+        return spec
+
+    def get_star(self, color):
+        mass = self.lut(color)
+        sparams = MainSequence.get_params(mass)
+        params = self.model.get_params(sparams)
+        star = self.model.get_star(*params)
+        return star
+
+    def __call__(self, color):
+        star = self.get_star(color)
+        return star
