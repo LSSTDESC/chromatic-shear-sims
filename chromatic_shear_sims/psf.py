92d68f3d442c15158101bd76a7d74eded583e84a
Sidney Mau <sidneymau@gmail.com>
2024-08-26 16:18:19 -0700

    update centering and scaling of psf image

--- chromatic_shear_sims/psf.py
+++ chromatic_shear_sims/psf.py
@@ -3,8 +3,6 @@ import logging
 
 import galsim
 
-from chromatic_shear_sims import utils
-
 
 logger = logging.getLogger(__name__)
 
@@ -29,12 +27,16 @@ class PSF:
         # note that we provide a special method for drawing PSF images to
         # ensure normalizaiton and centering
         psf_model = self.model
-        observed_psf = galsim.Convolve([star, psf_model])
+        observed_psf = galsim.Convolve([star, psf_model]).withFlux(
+            1,
+            throughput,
+        )
+        _centroid = observed_psf.calculateCentroid(throughput)
+        _offset = -1 * _centroid / image.scale
         observed_psf.drawImage(
             image=image,
             bandpass=throughput,
+            offset=_offset,
             add_to_image=True,
         )
-        rescaled_image = utils.rescale(image)
-        recentered_image = utils.recenter(rescaled_image)
-        return recentered_image
+        return image
