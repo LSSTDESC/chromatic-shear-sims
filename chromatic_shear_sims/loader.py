d3b9d975e22827ddc895c9c2f7412b641bef14f5
Sidney Mau <sidneymau@gmail.com>
2024-08-26 12:20:25 -0700

    update loader

--- chromatic_shear_sims/loader.py
+++ chromatic_shear_sims/loader.py
@@ -1,16 +1,11 @@
 import copy
 import logging
-import os
-import pickle
 import time
 
 import numpy as np
 import pyarrow as pa
 import pyarrow.compute as pc
 import pyarrow.dataset as ds
-from pyarrow import acero
-# import scipy.stats
-import yaml
 
 
 logger = logging.getLogger(__name__)
@@ -34,121 +29,20 @@ def parse_expression(predicate):
         return predicate
 
 
-def parse_projection(projection):
-    """Parse a projection from a dict of expressions or a list into a dict
-    """
-    projection_dict = {}
-    if projection is not None:
-        for proj in projection:
-            # if a projection is itself a dict, parse the expression as a pyarrow
-            # compute expression
-            if type(proj) == dict:
-                for k, v in proj.items():
-                    projection_dict[k] = parse_expression(v)
-            # else interpret as a simple field from the schema
-            else:
-                projection_dict[proj] = pc.field(proj)
-
-    return projection_dict
-
-
-def parse_options(options):
-    """Parse an options tree intro a pyarrow options object
-    """
-    # Parse through the tree
-    if type(options) is dict:
-        for k, v in options.items():
-            f = getattr(pc, k)
-            return f(**v)
-    else:
-        return options
-
-
 class Loader:
     def __init__(self, config):
         self.config = copy.copy(config)
         self.path = self.config.get("path")
+
+        logger.info(f"initializing loader for {self.path}")
+
         self.format = self.config.get("format")
         self.seed = self.config.get("seed", None)
 
         self.predicate_dict = self.config.get("predicate", None)
         self.predicate = parse_expression(self.predicate_dict)
 
-        self.projection_dict = self.config.get("projection", None)
-        self.projection = parse_projection(self.projection_dict)
-
-        self.aggregate_dict = self.config.get("aggregate", None)
-
-        self.num_rows = None
-
-        logger.info(f"initializing loader for {self.path}")
-
-    @classmethod
-    def from_yaml(cls, filename):
-        logger.info(f"initializing loader from {filename}")
-        with open(filename) as fp:
-            config = yaml.safe_load(fp)
-        return cls(config)
-
-    def do_aggregate(self, dataset, projection, predicate, aggregate):
-        """
-        Plan and execute aggregations for a dataset
-        """
-        scan_node = acero.Declaration(
-            "scan",
-            acero.ScanNodeOptions(
-                dataset,
-                columns=projection,
-                filter=predicate,
-            ),
-        )
-        if predicate is not None:
-            filter_node = acero.Declaration(
-                "filter",
-                acero.FilterNodeOptions(
-                    predicate,
-                ),
-            )
-        project_node = acero.Declaration(
-            "project",
-            acero.ProjectNodeOptions(
-                [v for k, v in projection.items()],
-                names=[k for k, v in projection.items()],
-            )
-        )
-        aggregate_node = acero.Declaration(
-            "aggregate",
-            acero.AggregateNodeOptions(
-                [
-                    (
-                        agg.get("input"),
-                        agg.get("function"),
-                        parse_options(agg.get("options", None)),
-                        agg.get("output"),
-                    )
-                    for agg in aggregate
-                ],
-            )
-        )
-        if predicate is not None:
-            seq = [
-                scan_node,
-                filter_node,
-                project_node,
-                aggregate_node,
-            ]
-        else:
-            seq = [
-                scan_node,
-                project_node,
-                aggregate_node,
-            ]
-        plan = acero.Declaration.from_sequence(seq)
-        logger.debug(plan)
-
-        res = plan.to_table(use_threads=True)
-
-        return res
+        self.num_rows = self.count_rows()
 
     def get_scanner(self, columns=None):
         """
@@ -162,34 +56,15 @@ class Loader:
 
         return scanner
 
-    def process(self):
+    def count_rows(self):
         """
         Process a dataset defined in a config
         """
-        dataset = ds.dataset(self.path, format=self.format)
-        num_rows = dataset.count_rows(filter=self.predicate)
-        self.num_rows = num_rows
-
-        logger.info(f"processing aggregates for {self.path}")
-        if self.aggregate_dict is not None:
-            aggregate = self.do_aggregate(
-                dataset,
-                self.projection,
-                self.predicate,
-                self.aggregate_dict,
-            )
-            aggregate_dict = aggregate.to_pydict()
-            aggregates = {}
-            for k, v in aggregate_dict.items():
-                if len(v) == 1:
-                    aggregates[k] = v[0]
-                else:
-                    aggregates[k] = v
-        else:
-            aggregates = None
-
-        self.aggregates = aggregates
-        return
+        # dataset = ds.dataset(self.path, format=self.format)
+        # num_rows = dataset.count_rows(filter=self.predicate)
+        scanner = self.get_scanner()
+        num_rows = scanner.count_rows()
+        return num_rows
 
     def get_rng(self, seed=None):
         rng_seed = seed if seed else self.seed
@@ -221,33 +96,4 @@ class Loader:
         logger.info(f"sampled {n} records from {self.path} in {_elapsed_time} seconds")
 
         return objs
-
-    def digitize_color(self, n_bins=100):
-        min_color = self.aggregates.get("min_color")
-        max_color = self.aggregates.get("max_color")
-
-        logger.info(f"making color bins ({min_color}, {max_color}, {n_bins})")
-        color_bin_edges = np.linspace(
-            min_color,
-            max_color,
-            n_bins + 1,
-        )
-        color_hist = np.zeros(n_bins)
-
-        logger.info(f"accumulating color histogram...")
-        scanner = self.get_scanner(columns=self.projection)
-        for i, batch in enumerate(scanner.to_batches()):
-            logger.debug(f"accumulating color histogram batch {i}")
-            _hist, _ = np.histogram(
-                batch["color"],
-                bins=color_bin_edges,
-            )
-            color_hist += _hist
-
-        return color_hist, color_bin_edges
-        # hist_dist = scipy.stats.rv_histogram(
-        #     (color_hist, color_bin_edges),
-        #     density=False,
-        # )
-        # return hist_dist
 
