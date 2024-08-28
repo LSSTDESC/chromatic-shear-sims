8f95706a00c42c6e52b3750066caa76b66f2b0e2
Sidney Mau <sidneymau@gmail.com>
2024-08-28 13:55:45 -0700

    update data module

--- src/chromatic_shear_sims/data.py
+++ src/chromatic_shear_sims/data.py
@@ -13,32 +13,10 @@ class Data:
         self.data_builder = utils.get_class(entrypoint)
         self.columns = self.data_builder.columns
         self.data_loader = data_loader
-        # self._data = None
-
-    # @property
-    # def data(self):
-    #     return self._data
-
-    # @property
-    # def num_rows(self):
-    #     return self.data.num_rows
-
-    # # TODO make this a random process that will iterate through data and load more if needed?
-    # def __call__(self, i, **kwargs):
-    #     return self.data.get_params(i, **kwargs)
 
     def load(self, n, seed=None):
         sample = self.data_loader.sample(n, columns=self.columns, seed=seed)
-        # self._data = self.data_builder(sample)
 
         data = self.data_builder(sample)
 
-        return Datum(data)
-
-
-class Datum:
-    def __init__(self, data):
-        self.data = data
-
-    def __call__(self, i, **kwargs):
-        return self.data.get_params(i, **kwargs)
+        return data
